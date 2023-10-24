c5d8c508f94a23658ebbb95d0cda494697aab14d
unknown <manojpremsm@gmail.com>
2023-10-24 14:15:42 +0200

    'logging'

---
+++ src/exceptions.py
@@ -0,0 +1,16 @@
+import sys
+import os 
+
+def error_message_details(error_msg,error_details):
+    -,-,exc_tb = sys.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+    file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+
+class custom_error_handler(Exception):
+    def __init__(self,error_msg,error_msg_details):
+        super().__init__(error_msg)
+        self.error_msg_details = error_message_details(error_msg,error_details)
